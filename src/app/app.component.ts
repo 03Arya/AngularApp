fd43a13f32798dbc577eaa223da71935b24cc845
arya <you@example.com>
2024-05-01 19:08:46 +0200

    made a powerup upgrade and removed demicals from total cash and animated the pickaxe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
   standalone: true
 })
+
 export class AppComponent {
   count = 0;
   clickPower = 1;
+  powerUpActive = false;
+  animate = false;
+
+  buyPowerUp() {
+    if (this.count >= 100) {
+      this.count /= 2;
+      this.powerUpActive = true;
+      this.clickPower *= 2;
+
+      setTimeout(() => {
+        this.powerUpActive = false;
+        this.clickPower /= 2;
+      }, 10000); // Power-up lasts for 10 seconds
+    }
+  }
 
   buyClickPower() {
     this.count -= 10;
@@ -35,7 +51,12 @@ export class AppComponent {
   }
 
   click() {
-    this.count += this.clickPower;
+    this.animate = true;
+    this.count = Math.trunc(this.count + this.clickPower);
+
+    setTimeout(() => {
+      this.animate = false;
+    }, 200); // remove the animate class after 0.2 seconds
   }
 }
 
